5b44e568f3615a60d968f7534701320bfd6d34b6
paidgeek <tin.rabzelj@gmail.com>
2016-03-21 14:57:14 +0100

    other aggregates

--- src/main/java/si/opkp/util/RequestColumn.java
+++ src/main/java/si/opkp/util/RequestColumn.java
@@ -1,20 +1,37 @@
 package si.opkp.util;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RequestColumn {
 
-	private static final String COUNT_SUFFIX = "$count";
+	private static final Map<String, Aggregate> SUFFIXES = new HashMap<String, Aggregate>() {{
+		put("$avg", Aggregate.AVG);
+		put("$count", Aggregate.COUNT);
+		put("$count_distinct", Aggregate.COUNT_DISTINCT);
+		put("$max", Aggregate.MAX);
+		put("$min", Aggregate.MIN);
+		put("$sum", Aggregate.SUM);
+		put("$std", Aggregate.STD);
+		put("$std_sample", Aggregate.STD_SAMPLE);
+		put("$var", Aggregate.VAR);
+		put("$var_sample", Aggregate.VAR_SAMPLE);
+	}};
 
 	private String name;
 	private Aggregate aggregate;
 
 	public RequestColumn(String name) {
-		if (name.endsWith(COUNT_SUFFIX)) {
-			aggregate = Aggregate.COUNT;
-			this.name = name.substring(0, name.length() - COUNT_SUFFIX.length());
-		} else {
-			aggregate = Aggregate.NONE;
-			this.name = name;
-		}
+		this.name = name;
+		this.aggregate = Aggregate.NONE;
+
+		SUFFIXES.keySet()
+				  .stream()
+				  .filter(name::endsWith)
+				  .forEach(key -> {
+					  aggregate = SUFFIXES.get(key);
+					  this.name = name.substring(0, name.length() - key.length());
+				  });
 	}
 
 	public RequestColumn(String name, Aggregate aggregate) {
